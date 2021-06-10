cb82b721b6150de7477c152fd8010b7c7b69cc13
saathvikdm <saathvikdm@outlook.com>
2021-06-10 19:52:57 +0530

    improved styles

--- js/components/getComponents.js
+++ js/components/getComponents.js
@@ -1,11 +1,17 @@
 const getCardWithThumb = (title, desc, img) => {
-  return `<div class="conf-box">
-                    <img class="recent-conf" src="${img}" alt="">
-                    <div class="conf-box-details">
-                      <h4 class="conf-box-title">${title}</h4>
-                      <p class="conf-box-desc">${desc}</p>
-                    </div>
-                  </div>`;
+  return `<div class="card mb-3 box-shadow-md" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-6">
+                <img src="${img}" width="100%" height="180" alt="...">
+              </div>
+              <div class="col-md-6">
+                <div class="card-body">
+                  <h5 class="card-title">${title}</h5>
+                  <p class="card-text">${desc}</p>
+                </div>
+              </div>
+            </div>
+          </div>`;
 };
 
 const getBlockWithoutImage = (title, desc) => {
@@ -40,10 +46,12 @@ const getBlockWithImage = (title, desc, img) => {
 };
 
 const getRecentWorkList = (title, desc) => {
-  return `<div class="work-list" style="margin-top: 1em; margin-bottom: 1em;">
-              <h6>${title}</h6>
-              <p>${desc}</p>
-            </div>`;
+  return `<div class="card bg-light mb-3 box-shadow-md" style="max-width: 540px;">
+            <h5 class="card-header">${title}</h5>
+            <div class="card-body">
+              <p class="card-text">${desc}</p>
+            </div>
+          </div>`;
 };
 
 export {
