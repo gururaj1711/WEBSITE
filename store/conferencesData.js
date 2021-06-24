cd2e8c52ed2175e4a4a95b5220756fab5786d6f8
Gururaj H L <48173624+gururaj1711@users.noreply.github.com>
2021-06-24 11:36:53 +0530

    Update conferencesData.js

--- store/conferencesData.js
+++ store/conferencesData.js
@@ -14,4 +14,19 @@ export const recentConferences = [
     desc: "SAGE University, Bhopal",
     confImg: "./img/NIT%20Surat%20(1).jpg",
   },
+  {
+    title: "ACM DSP Talk",
+    desc: "UEM-Jaipur",
+    confImg: "./img/UEM-Jaipur.jpeg,
+  },
+  {
+    title: "Expert Talk",
+    desc: "Sri Narayana Guru, Tamil Nadu",
+    confImg: "./img/GURU-TN.jpeg",
+  },
+  {
+    title: "IndiaClouds",
+    desc: "First Pitching Event: Episode #1",
+    confImg: "./img/Indiaclouds.jpeg",
+  },
 ];
