01fe60dabef2c384b2473aa5a10fec4817d68210
Shane Chellam S P <shanechellamsp@gmail.com>
2025-02-18 13:13:34 +0530

    Updation of Works

--- store/conferencesData.js
+++ store/conferencesData.js
@@ -44,4 +44,114 @@ export const recentConferences = [
     desc: "Content and delivery of Blockchain course at Maryland University, Sweden",
     confImg: "./img/Course%20Content.jpeg",
   },
+  {
+    title: "Speaker",
+    desc: "Asia Pacific Next Generation(APNG) Webminar",
+    confImg: "./img/APAC.jpeg",
+  },
+  {
+    title: "Chief Guest and Resource Person",
+    desc: "Vidya Sanskar College, Bengaluru",
+    confImg: "./img/CgRp.jpeg",
+  },
+  {
+    title: "Resource Person",
+    desc: "VRSEC, Vijayawada",
+    confImg:"./img/VRSEC.jpeg",
+  },
+  {
+    title: "Speaker",
+    desc: "The Neotia Univeristy, Sarisha, West Bengal",
+    confImg: "./img/TNU.jpeg",
+  },
+  {
+    title: "Guest Speaker",
+    desc: "ECU, Australia",
+    confImg: "./img/ecu.jpeg",
+  },
+  {
+    title: "Speaker",
+    desc: "Taylors University, Malaysia",
+    confImg: "./img/Taylors.jpeg",
+  },
+  {
+    title: "Speaker",
+    desc: "CGPIT, UTU",
+    confImg: "./img/Dst.jpeg",
+  },
+  {
+    title: "Speaker",
+    desc: "BNMIT, Bengaluru",
+    confImg: "./img/BNMIT.jpeg",
+  },
+  {
+    title: "FDP-Resource Person",
+    desc: "SVCE, Bengaluru",
+    confImg: "./img/SVCE.jpeg",
+  },
+  {
+    title: "Resource Person",
+    desc: "AIT Tumkur",
+    confImg: "./img/AIT.jpeg",
+  },
+  {
+    title: "Chief Guest",
+    desc: "International Gamathon - Jyothi Institute of Technology, Bengaluru",
+    confImg: "./img/JIT.jpeg",
+  },
+  {
+    title: "Chiel Guest",
+    desc: "Inaugral Ceremony of ACM Student Chapter, NMIT, Bengaluru",
+    confImg: "./img/NMIT.jpeg",
+  },
+  {
+    title: "FDP-Resource Person",
+    desc: "Dayananda Sagar College of Engineering, Bengaluru",
+    confImg: "./img/DSCE.jpeg",
+  },
+  {
+    title: "Guest of Honour",
+    desc: "ICCIIS-2024, BNMIT, Bengaluru",
+    confImg: "./img/ICCIIS2024.jpeg",
+  },
+  {
+    title: "Program Chair",
+    desc: "CODE-AI 2024, MIT, Bengaluru",
+    confImg: "./img/codeai.jpeg",
+  },
+  {
+    title: "Chief Guest & Keynote Speaker",
+    desc: "DST-SERB, RMK Engineering College, Chennai",
+    confImg: "./img/RMK.jpeg",
+  },
+  {
+    title: "CFP for Special Session",
+    desc: "3rd International Conference on Sensing, Measurement, Communication, and Internet of Things Technologies (SMC-IoT)",
+    confImg: "./img/SMC.png"
+  },
+  {
+    title: "Session Chair",
+    desc: "Elsevier International Conference on Machine Learning and Data Engineering (ICMLDE 3.0)",
+    confImg: "./img/ICMLDE.jpeg"
+  },
+  {
+    title: "Session Chair",
+    desc: "IEEE ANTS 2024, IIT Guwahati",
+    confImg: "./img/IEEEANTS.jpeg"
+  },
+  {
+    title: "Delegate",
+    desc: "Blockchain in E-Health Workshop, IIT Kanpur",
+    confImg: "./img/IITK.jpeg"
+  },
+  {
+    title: "Chief Guest",
+    desc: "Inaugural Ceremony of ACM Student Chapter, BNMIT, Bengaluru",
+    confImg: "./img/ACMBNMIT.jpeg"
+  },
+  {
+    title: "Keynote Speaker",
+    desc: "ESP Session, PCCOE, Pune",
+    confImg: "./img/PCCOE.jpeg"
+  }
 ];
--- store/worksData.js
+++ store/worksData.js
@@ -71,6 +71,124 @@ export const recentWorks = [
     desc: "AI for Healthcare Applications",
     img: "./img/WhatsApp Image 2024-03-12 at 6.16.41 PM.jpeg",
   },
+  {
+    title: "Australian Patent: 2021102665",
+    desc: "Advanced WI-FI Enable Water Surface Cleaner"
+  },
+  {
+    title: "Analysis of Finger Vein Recognition using Deep Learning Techniques",
+    desc: "HL Gururaj, Nydile M N, Francesco Flammini, Vidyashree K P, Soundarya B C, “Association of Computer Machinery”",
+  },
+  {
+    title: "Statistical Channel Model and Systematic Random Linear Network Coding Based QoS Oriented and Energy Efficient UWSN Routing Protocol",
+    desc: "HL Gururaj, Francesco Flammini, Noor Zaman, IF=2.69 “MDPI”"
+  },
+  {
+    title: "Call for Book Chapters",
+    desc: "Federated Learning and its Application in the Healthcare Industry",
+    img: "./img/Fedlearning.jpeg",
+  },
+  {
+    title: "Analysis of Preventive Measures Against DDoS Attacks in Smart Grids",
+    desc: "HL Gururaj, BH Swathi, R Trupti, R Darshan, AB Rajendra, K Paramesha, IF=1.33 “Springer”",
+  },
+  {
+    title: "Efficient SCAN and Chaotic Map Encryption System for Securing E-Healthcare Images",
+    desc: "HL Gururaj, Kiran, Meshari Almeshari, Yasser Alzamil, Vinayakumar Ravi, KV Sudeesh, IF=4.2 “MDPI”",
+  },
+  {
+    title: "System and Method for Driver Drowsiness Detection Using Behavioral and Sensor-Based Physiological Measures",
+    desc:"HL Gururaj, Jaspreet Singh Bajaj, Naveen Kumar, Rajesh Kumar Kaushal, Francesco Flammini, Rajesh Natarajan, “MDPI Sensor Q1 Journal IF=3.84 SCI Indexed”",
+  },
+  {
+    title: "A Novel Framework on Security and Energy Enhancement Based on Internet of Medical Things for Healthcare 5.0",
+    desc: "HL Gururaj, Rajesh Natarajan, Francesco Flammini, Anitha Premkumar, Vinoth Kumar Venkatesan, Shashi Kant Gupta, “MDPI Infrastructures Q2 Journal IF=3.00 SCI Indexed”",
+  },
+  {
+    title: "Sentiment analysis on cross-domain textual data using classical and deep learning approaches",
+    desc: "HL Gururaj, “Springer MTAP Q1 Journal IF=2.55 SCI Indexed”",
+  },
+  {
+    title: "Fault detection and state estimation in robotic automatic control using machine learning",
+    desc: "HL Gururaj, Rajesh Natarajan, Francesco Flammini, Santosh Reddy P, Subash Chandra Bose, Shanmugapriya Velmurugan, “Elseiver Q1 Journal ”",
+  },
+  {
+    title: "DeepSkin: A Deep Learning Approach for Skin Cancer Classification",
+    desc: "HL Gururaj, N Manju, A Nagarjun, VN Manjunath Aradhya, Francesco Flammini, “IEEE Access, vol. 11, Q1 Journal, SCI indexed IF-3.67”",
+  },
+  {
+    title: "Collaborative energy-efficient routing protocol for sustainable communication in 5G/6G wireless sensor networks",
+    desc: "HL Gururaj, Francesco Flammini, “IEEE IF-8.00, SCI indexed and Q1 Journal”",
+  },
+  {
+    title: "A Novel Application of K-means Cluster Prediction Model for Diabetes Early Identification using Dimensionality Reduction Techniques",
+    desc: "HL Gururaj, Vamshi Krishna, Raguru Jaya K, Bhuvaneswari AP, Vinayakumar Ravi, Meshari Almeshari, Yasser Alzamil “The Open Bioinformatics Journal”",
+  },
+  {
+    title: "Call for Book Chapters",
+    desc: "Artificial Intelligence for Military Applications with Blockchain",
+    img: "./img/AIMilitary.jpeg",
+  },
+  {
+    title: "Machine learning based 64-QAM classification techniques for enhanced optical communication",
+    desc: "HL Gururaj, Francesco Flammini, P Kiran, DS Sunil Kumar, V Veeraprathap, “Springer Optical and Quantum Electronics Journal, SCIE Indexed Q2 rated Journal with IF-2.77”",
+  },
+  {
+    title: "Electronic Health Record (EHR) System Development for Study on EHR Data-based Early Prediction of Diabetes Using Machine Learning Algorithms",
+    desc: "HL Gururaj, Jagadamba G, Shashidhar R, Vinayakumar Ravi, Meshari Almeshari, Yasser Alzamil, “The Open Bioinformatics Journal, Scopus Indexed”",
+  },
+  {
+    title: "Call for Book Chapters",
+    desc: "6G Cyber Security Resilience:Trends and Challenges",
+    img: "./img/6G.jpeg",
+  },
+  {
+    title: "Call For Book chapter",
+    desc: "CRC Press, Taylor & Francis Group (indexed in Scopus) Edited Book on Cyber Security",
+    img: "./img/csba.jpeg",
+  },
+  {
+    title: "Synchronization and implementation of a real-time traffic signal optimization regulator",
+    desc: "HL Gururaj, M Dakshayini, Abhilasha Varshney, Yu-Chen Hu, “MTAP Springer Journal with IF-3.6”",
+  },
+  {
+    title: "Classification of Various Plant Leaf Disease Using Pretrained Convolutional Neural Network On Imagenet",
+    desc: "HL Gururaj, Geetabai S Hukkeri, BC Soundarya, Vinayakumar Ravi, “The Open Agriculture Journal”",
+  },
+  {
+    title: "Application of MPSO Algorithm for Optimal Route Selection in Internet of Things",
+    desc: "7th International Conference on Control, Robotics, and Informatics (ICCRI 2024), held at Duy Tan University, Danang, Vietnam",
+    img: "./img/ICCRI.jpeg",
+  },
+  {
+    title: "A Comprehensive Review of Face Recognition Techniques, Trends, and Challenges",
+    desc: "HL Gururaj, Soundarya BC, Shreyas J, Francesco Flammini, “IEEE Access, Q1 Journal with IF-3.7 and indexed in Science Citation Index Expanded-SCIE (Clarivate Analytics) & Scopus”",
+  },
+  {
+    title: "Call for Book Chapters",
+    desc: "Artificial Intelligence based Drone of Things : Springer",
+    img: "./img/Drone.jpeg",
+  },
+  {
+    title: "Call for Book Chapters",
+    desc: "Artificial Intelligence-driven Waste Management and Circular Economy",
+    img: "./img/wasteman.jpeg"
+  },
+  {
+    title: "Split Federated Learning for Secure IoT Applications: Concepts, frameworks, applications and case studies",
+    desc: "HL Gururaj, Geetabai S Hukkeri, Noor Zaman, Hong Lin “The Institution of Engineering and Technology(IET)”",
+    img: "./img/IET.jpeg",
+  },
+  {
+    title: "Call for Book Chapters",
+    desc:"Coding Theory and Cryptographic Applications in Cyber Security",
+    img: "./img/crypto.jpeg",
+  }, 
+  {
+    title: " Call for Book Chapters",
+    desc: "Reliability in Cyber-Physical systems: The human factor perscpective ",
+    img: "./img/cybphy.jpeg",
+  }
   // {
   //   title: "Work with image",
   //   desc: "lorem description",
